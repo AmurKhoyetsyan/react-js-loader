f622dedc5788bc613f4cad1298140b8a23086766
AmurKhoyetsyan <javanewss@gmail.com>
2021-01-04 14:42:58 +0400

    replace pure js and readme md from pure js and babel js

--- src/loader/PureJsForPublish/index.js
+++ src/loader/PureJsForPublish/index.js
@@ -7,32 +7,68 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _Box = _interopRequireDefault(require("./Box"));
-
 var _Heart = _interopRequireDefault(require("./Heart"));
 
-var _Bubble = _interopRequireDefault(require("./Bubble"));
+var _Box = _interopRequireDefault(require("./Box/Box1"));
 
-var _Spinner = _interopRequireDefault(require("./Spinner"));
+var _Box2 = _interopRequireDefault(require("./Box/Box2"));
+
+var _Box3 = _interopRequireDefault(require("./Box/Box3"));
+
+var _Bubble = _interopRequireDefault(require("./Bubble/Bubble1"));
+
+var _Bubble2 = _interopRequireDefault(require("./Bubble/Bubble2"));
+
+var _Spinner = _interopRequireDefault(require("./Spinner/Spinner1"));
+
+var _Spinner2 = _interopRequireDefault(require("./Spinner/Spinner2"));
+
+var _Spinner3 = _interopRequireDefault(require("./Spinner/Spinner3"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Loader = function Loader(props) {
+  console.log(props);
+
   switch (props.type) {
-    case "box":
+    case "box-rotate-x":
       return _react.default.createElement(_Box.default, props);
+      break;
+
+    case "box-rotate-y":
+      return _react.default.createElement(_Box2.default, props);
+      break;
+
+    case "box-rotate-z":
+      return _react.default.createElement(_Box3.default, props);
+      break;
 
     case "heart":
       return _react.default.createElement(_Heart.default, props);
+      break;
 
-    case "bubble":
+    case "bubble-scale":
       return _react.default.createElement(_Bubble.default, props);
+      break;
 
-    case "spinner":
+    case "bubble-top":
+      return _react.default.createElement(_Bubble2.default, props);
+      break;
+
+    case "spinner-cub":
       return _react.default.createElement(_Spinner.default, props);
+      break;
+
+    case "spinner-circle":
+      return _react.default.createElement(_Spinner2.default, props);
+      break;
+
+    case "spinner-default":
+      return _react.default.createElement(_Spinner3.default, props);
+      break;
 
     default:
-      return _react.default.createElement(_Spinner.default, props);
+      return _react.default.createElement(_Spinner3.default, props);
   }
 };
 
